e81e950e4bcc0d3ef6cd36c2faa77f79ffa9ae36
Carlos F. R. Pires <cfrpires@gmail.com>
2023-05-15 19:09:11 -0300

    arrumação do carlos

--- script/main.js
+++ script/main.js
@@ -130,7 +130,7 @@ const animationTimeline = () => {
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=11")
     .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=10")
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=13")
     .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
@@ -138,9 +138,9 @@ const animationTimeline = () => {
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff"
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=12")
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=15")
     .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=12")
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=15")
     .from(
       ".idea-5",
       0.7,
